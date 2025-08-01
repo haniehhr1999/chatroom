b98a039d6e9c69622dadf2ac8980a97c19c12b80
hanieh <haniehrahim78@gmail.com>
2025-08-01 22:35:15 +0330

    get pvs and show each pv in li tag

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,27 @@
+"use client";
 import Image from "next/image";
 import man from "../../public/images/man.jpg";
 import { FaUserLarge } from "react-icons/fa6";
+import IO from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = IO.connect("http://localhost:4000");
 
 export default function Home() {
+  const [pvs, setPvs] = useState([]);
+  useEffect(() => {
+    socket.on("privateChats", (pvs) => {
+      setPvs([...pvs]);
+    });
+  }, []);
   return (
     <div className="grid grid-cols-10 h-screen">
       <div className="bg-[#dfe8e3] p-5 col-span-3">
         <ul>
           <li className="flex bg-white rounded-md px-4 py-2">
             <div className="avatar-box">
-              <Image width={50} src={man} className="avatarr"  />
-              <div className="offline">
-
-              </div>
+              <Image width={50} src={man} className="avatarr" />
+              <div className="offline"></div>
             </div>
             <div className="mx-3">
               <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
@@ -24,10 +33,8 @@ export default function Home() {
 
           <li className="flex bg-white rounded-md px-4 py-2">
             <div className="avatar-box">
-              <Image width={50} src={man} className="avatarr"  />
-              <div className="offline">
-
-              </div>
+              <Image width={50} src={man} className="avatarr" />
+              <div className="offline"></div>
             </div>
             <div className="mx-3">
               <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
@@ -39,10 +46,8 @@ export default function Home() {
 
           <li className="flex bg-white rounded-md px-4 py-2">
             <div className="avatar-box">
-              <Image width={50} src={man} className="avatarr"  />
-              <div className="offline">
-
-              </div>
+              <Image width={50} src={man} className="avatarr" />
+              <div className="offline"></div>
             </div>
             <div className="mx-3">
               <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
@@ -54,10 +59,8 @@ export default function Home() {
 
           <li className="flex bg-white rounded-md px-4 py-2">
             <div className="avatar-box">
-              <Image width={50} src={man} className="avatarr"  />
-              <div className="offline">
-
-              </div>
+              <Image width={50} src={man} className="avatarr" />
+              <div className="offline"></div>
             </div>
             <div className="mx-3">
               <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
@@ -69,10 +72,8 @@ export default function Home() {
 
           <li className="flex bg-white rounded-md px-4 py-2">
             <div className="avatar-box">
-              <Image width={50} src={man} className="avatarr"  />
-              <div className="offline">
-
-              </div>
+              <Image width={50} src={man} className="avatarr" />
+              <div className="offline"></div>
             </div>
             <div className="mx-3">
               <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
@@ -84,10 +85,8 @@ export default function Home() {
 
           <li className="flex bg-white rounded-md px-4 py-2">
             <div className="avatar-box">
-              <Image width={50} src={man} className="avatarr"  />
-              <div className="offline">
-
-              </div>
+              <Image width={50} src={man} className="avatarr" />
+              <div className="offline"></div>
             </div>
             <div className="mx-3">
               <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
@@ -96,6 +95,22 @@ export default function Home() {
               </p>
             </div>
           </li>
+
+          {pvs.map((pv) => (
+            <li className="flex bg-white rounded-md px-4 py-2">
+              <div className="avatar-box">
+                <Image width={50} src={man} className="avatarr" />
+                <div className="offline"></div>
+              </div>
+              <div className="mx-3">
+                <h3 className="text-[#52796f] text-xl bold">محمدحسین طوافی</h3>
+                <p className="text-[#b1b1b1]">
+                  این یک متن تستی برای تست است صرفا
+                </p>
+              </div>
+            </li>
+          ))}
+          
         </ul>
       </div>
       <div className=" col-span-7 ">
